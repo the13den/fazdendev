0b936093a5d160f5b293575359c04e18b0e0917c
webfrontden <87566853+fazdendev@users.noreply.github.com>
2022-08-01 00:01:10 +0500

    Improved form validation errors appearance
    Improved form invalid filling errors appearance now you can see what you should do to fix it (for example: Enter the correct email addres), form sending process was also prettified.
    Улучшен внешний вид ошибок неправильного заполнения формы, теперь вы можете видеть, что вы должны сделать, чтобы исправить это (например: Введите правильный адрес электронной почты), визуальная часть процесса отправки формы также была улучшена.

--- js/app.js
+++ js/app.js
@@ -82,56 +82,106 @@
         if (userAgent.includes("irefox")) adaptiveSizePageScale();
     }
     firefoxAdaptiveSizePageScale();
-    function validateForm() {
-        let error = 0;
-        let formReq = document.querySelectorAll("._req");
-        for (let index = 0; index < formReq.length; index++) {
-            const input = formReq[index];
-            console.log(input);
-            inputRemoveError(input);
-            if ("" === input.value || " " === input.value) {
-                inputAddError(input);
-                error++;
-            } else if (input.classList.contains("_email")) if (false == validateEmail(input)) {
-                inputAddError(input);
-                error++;
-            }
-        }
-        return error;
-    }
-    function inputAddError(input) {
-        input.parentElement.classList.add("_error");
-        input.classList.add("_error");
-    }
-    function inputRemoveError(input) {
-        input.parentElement.classList.remove("_error");
-        input.classList.remove("_error");
-    }
-    function validateEmail(email) {
-        var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-        var address = email.value;
-        if (false == reg.test(address)) return false;
-    }
     new SmoothScroll('a[href*="#"]', {
         speed: 300
     });
+    if (localStorage.formSent) ;
+    let todayDate = new Date;
+    let todayDay = todayDate.getDate();
+    let todayMonth = todayDate.getMonth();
     function initForm() {
         let formButton;
+        let forms = document.querySelectorAll(".feedback-form");
         let form;
+        let localFormSent;
+        if (localStorage.formSent) localFormSent = JSON.parse(localStorage.formSent); else localFormSent = 0;
+        formSentDailyReset();
+        function formSentDailyReset() {
+            if (localFormSent.day != todayDay || localFormSent.month != todayMonth) localStorage.formSent = JSON.stringify({
+                value: 0,
+                day: todayDay,
+                month: todayMonth
+            });
+        }
+        for (let index = 0; index < forms.length; index++) {
+            let form = forms[index];
+            checkFormSent(form);
+        }
+        function checkFormSent(form) {
+            if (localFormSent.value) addClassFormSent(form); else removeClassFormSent(form);
+            function addClassFormSent(form) {
+                form.classList.add("_form-sent");
+            }
+            function removeClassFormSent(form) {
+                form.classList.remove("_form-sent");
+            }
+        }
+        function validateForm(form) {
+            let error = 0;
+            let formReq = document.querySelectorAll("._req");
+            for (let index = 0; index < formReq.length; index++) {
+                const input = formReq[index];
+                if (form.contains(input)) {
+                    inputRemoveBlankError(input);
+                    inputRemoveEmailError(input);
+                    if ("" === input.value || " " === input.value) {
+                        inputAddBlankError(input);
+                        error++;
+                    } else if (input.classList.contains("_email")) if (false == validateEmail(input)) {
+                        inputAddEmailError(input);
+                        error++;
+                    }
+                }
+            }
+            return error;
+            function inputAddBlankError(input) {
+                input.parentElement.classList.add("_blank-error");
+                input.classList.add("_blank-error");
+            }
+            function inputRemoveBlankError(input) {
+                input.parentElement.classList.remove("_blank-error");
+                input.classList.remove("_blank-error");
+            }
+            function inputAddEmailError(input) {
+                input.parentElement.classList.add("_email-error");
+                input.classList.add("_email-error");
+            }
+            function inputRemoveEmailError(input) {
+                input.parentElement.classList.remove("_email-error");
+                input.classList.remove("_email-error");
+            }
+            function validateEmail(email) {
+                var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+                var address = email.value;
+                if (false == reg.test(address)) return false;
+            }
+        }
         document.addEventListener("click", (function(e) {
             if (e.target.closest(".feedback-form__button")) {
                 e.preventDefault();
                 formButton = e.target.closest(".feedback-form__button");
                 form = formButton.closest(".feedback-form");
-                let error = validateForm();
+                let error = validateForm(form);
                 if (0 === error) {
                     let formData = new FormData(form);
-                    alert("Отправка начата");
                     let xhr = new XMLHttpRequest;
                     xhr.open("POST", "https://script.google.com/macros/s/AKfycbwz2VYI8q092-TvasLvhEn3cdKaBFpNYFyVwFUXzqea2kvjctVC5cXlda_eIaa9ZOSF/exec");
                     xhr.send(formData);
+                    form.classList.add("_form-sending");
                     xhr.onload = function() {
-                        if (200 != xhr.status) alert(`Ошибка Отравки`); else alert(`Форма отправленна`);
+                        if (200 != xhr.status) {
+                            form.classList.remove("_form-sending");
+                            alert(`Ошибка Отравки`);
+                        } else {
+                            form.classList.remove("_form-sending");
+                            localStorage.formSent = JSON.stringify({
+                                value: 1,
+                                day: todayDay,
+                                month: todayMonth
+                            });
+                            localFormSent = JSON.parse(localStorage.formSent);
+                            checkFormSent(form);
+                        }
                     };
                 }
             }
